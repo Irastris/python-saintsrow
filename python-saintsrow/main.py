a6c436ff3d2c2a761724402758846879cb109dfb
Irastris <13782239+Irastris@users.noreply.github.com>
2022-08-29 01:06:48 -0400

    Move project to subfolder

---
+++ python-saintsrow/main.py
@@ -0,0 +1,14 @@
+import click
+
+from commands import extract
+from commands import search
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(extract.extract)
+cli.add_command(search.search)
+
+if __name__ == "__main__":
+    cli()
